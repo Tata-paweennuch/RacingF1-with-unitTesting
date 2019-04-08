f408cb5db962fac8cb5ea1499e096769a58d8a3b
unknown <paweennuch.boonamnuay@gmail.com>
2019-04-08 18:09:04 +0300

    complete version1

--- static/js/controllers/team.js
+++ static/js/controllers/team.js
@@ -6,21 +6,16 @@ angular
         let teamId = $routeParams.id; 
 
         $scope.allDrivers = dataService.getDrivers();
-        let sortedAllDrivers = $scope.allDrivers.sort((a, b) => {
-            if(a.points > b.points) return -1;
-            if(a.points < b.points) return 1;
-            if(a.driver > b.driver) return 1;
-            if(a.driver < b.driver) return -1;
-        });
+       
+        let sortedAllDrivers = $filter('orderBy')($scope.allDrivers, ['-points', 'driver']);
 
         let filteredDriversInTeam = $filter('filter')(sortedAllDrivers, function (driverObj) {
             if(driverObj.team == teamId)
             return driverObj;
         });
-        // $scope.teamDrivers = $scope.allDrivers.filter(driver => driver.team === parseInt(teamId))
 
         $scope.teamDrivers = filteredDriversInTeam;
-        console.log($scope.teamDrivers);        
+        // console.log($scope.teamDrivers);        
 
         dataService.getTeamDetail(teamId, function(response) {
             $scope.teamDetail = response.data;
